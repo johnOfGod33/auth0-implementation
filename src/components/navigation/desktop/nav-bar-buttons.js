71e1534aedbd979779cfc1deee0304e7abd0d7c6
jean de dieu <jeandedieusessou@gmail.com>
2024-09-07 13:00:58 +0000

    Implement Login and Logout with your Auth0

--- src/components/navigation/desktop/nav-bar-buttons.js
+++ src/components/navigation/desktop/nav-bar-buttons.js
@@ -1,5 +1,27 @@
 import React from "react";
-
+import { useAuth0 } from "@auth0/auth0-react";
 export const NavBarButtons = () => {
-  return <div />;
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+  const button = isAuthenticated ? (
+    <button
+      onClick={() =>
+        logout({ logoutParams: { returnTo: window.location.origin } })
+      }
+    >
+      Log out
+    </button>
+  ) : (
+    <button
+      onClick={() => {
+        console.log(window.location.origin);
+        loginWithRedirect({
+          redirectUri: `${window.location.origin}/profile`,
+        });
+      }}
+    >
+      Log in
+    </button>
+  );
+
+  return <div>{button}</div>;
 };
--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./app";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./styles/styles.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{ redirect_uri: window.location.origin }}
+      >
+        <App />
+      </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
